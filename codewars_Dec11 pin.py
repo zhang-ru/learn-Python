c56773e652f88b6434f73482c25bbf1087e61a78
zhang <ru.zhang@outlook.com>
2016-12-11 22:22:07 +0800

    codewars Dec11 pins updated with answer

--- codewars_Dec11 pin.py
+++ codewars_Dec11 pin.py
@@ -13,4 +13,12 @@ def get_pins(observed):
 	# return result
 	# return list(itertools.product("124",['12','24']))
 
+#ansewer:
+from itertools import product
+
+ADJACENTS = ('08', '124', '2135', '326', '4157', '52468', '6359', '748', '85790', '968')
+
+def get_pins(observed):
+    return [''.join(p) for p in product(*(ADJACENTS[int(d)] for d in observed))]
+
 print(get_pins("8"))
